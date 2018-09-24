eef21bb7ef2f260ecd83c253105c01b272fae80e
Ido Yaari <suzyido@yahoo.com>
2018-09-24 08:24:26 +0300

    Added routes for create user, login, and remove token using jwt

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 require('./config/config');
 
+const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Option} = require('./models/option');
 const express = require('express');
 var bodyParser = require('body-parser');
+
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -28,6 +32,44 @@ app.post('/options', (req, res) => {
             res.status(400).send(err);
         }
     });
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }, (err) => {
+        if(err) {
+            res.status(400).send(err);
+        }
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    })    
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);    
+        });
+   }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,31 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const {Option} = require('./../../models/option');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [
+{
+    _id: userOneId,
+    email: 'suzyido@yahoo.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
+    }]
+}, 
+{
+    _id: userTwoId,
+    email: 'suzyido1@yahoo.com',
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
+    }]
+}];
 
 const options = [
 {
@@ -19,8 +44,19 @@ const populateOptions = (done) => {
       return Option.insertMany(options);
     }).then(() => done());
 };
+  
+const populateUsers = (done) => {
+    User.deleteMany({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
 
 module.exports = {
     options,
-    populateOptions
+    populateOptions,
+    users,
+    populateUsers
 };
